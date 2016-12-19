a8eb81c65d28817133d5da793581fdefe9105fd2
phiresky <phireskyde+git@gmail.com>
2016-12-19 01:44:39 +0100

    add outputs

---
+++ trainNN/out/v036-online-lstm-dirty/network_model.py
@@ -0,0 +1,15 @@
+import lasagne
+from lasagne.layers import InputLayer, DenseLayer, DropoutLayer, LSTMLayer
+
+
+def create_network(config, BATCH_SIZE):
+    input_dim = config['input_dim']
+    num_labels = config['num_labels']
+    input_layer = InputLayer(shape=(BATCH_SIZE, input_dim / 2, 2))
+    input_var = input_layer.input_var
+    hidden_layer_1 = LSTMLayer(input_layer, 100)
+    hidden_layer_2 = LSTMLayer(hidden_layer_1, 50, only_return_final=True)
+    output_layer = DenseLayer(hidden_layer_2,
+                              num_units=num_labels,
+                              nonlinearity=lasagne.nonlinearities.softmax)
+    return locals()
