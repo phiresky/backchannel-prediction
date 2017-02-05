6069bf7a084e3f757088f6afb05e3ea90bffceee
phiresky <phiresky@isl>
2017-02-05 19:22:44 +0100

    sub scripts

--- submit-evaluate.sh
+++ submit-evaluate.sh
@@ -1,3 +1,3 @@
 for conf in "$@"
-do sbatch --partition=HPC,UBUNTU --job-name="eval-bc-$conf" --time=2-0 --cpus-per-task=64 --mem-per-cpu=6G ./DO.cpu.srun.sh evaluate.evaluate $conf
+do sbatch --partition=HPC,UBUNTU --job-name="eval-bc-$conf" --time=2-0 --cpus-per-task=4 --mem-per-cpu=20G ./DO.cpu.srun.sh evaluate.evaluate $conf
 done
