e1cb64260fa2049fe0ddcea3f048f0d2576b5be6
phiresky <phiresky@isl>
2017-02-05 19:25:42 +0100

    add eval gpu script

--- submit-eval-gpu.sh
+++ submit-eval-gpu.sh
@@ -1,7 +1,7 @@
 set -e
 
 for config in "$@"; do
-	confname="$(dirname "$config")"
+	confname="eval:$(basename "$(dirname "$config")")"
 	echo batching $config
 	sbatch --mem=10000M --partition=lowGPU,highGPU --job-name="$confname" --gres=gpu:1 --time=3-0 ./DO.srun.sh evaluate.evaluate "$config"
 done
