fb27b93fb1052fd8bf5fdfef4a44d0a9231dc5b7
phiresky <phireskyde+git@gmail.com>
2017-02-26 00:40:21 +0100

    update repro script

--- scripts/reproduce.sh
+++ scripts/reproduce.sh
@@ -6,18 +6,35 @@ if [[ ! -d configs/finunified ]]; then
     echo "you must run this script from the repository root"
     exit 1
 fi
+
+echo "See the Section 'Evaluation Visualizer' in the readme to see the results"
+
+
 echo "regenerating configs"
 rm -r configs/finunified/vary-*
 ts-node --fast configs/meta_config_generator.ts
 
-for config in configs/finunified/vary-*/*.json; do
-    echo "reproducing results for config $config"
-    python -m trainNN.train "$config"
+getOutVersion() {
+    config=$1
     gitversion="$(git describe --dirty)"
     configname="$(jq -r .name "$config")"
-    version="$gitversion:$configname"
+    echo "$gitversion:$configname"
+}
+
+bestconfig="configs/finunified/vary-features/lstm-best-features-power,pitch,ffv,word2vec_dim30.json"
+
+echo "reproducing best config $bestconfig"
+python -m trainNN.train "$bestconfig"
+bestversion="$(getOutVersion "$bestconfig")"
+echo "$bestversion"
+python -m evaluate.evaluate "trainNN/out/$bestversion/config.json" allmargins
+
+for config in configs/finunified/vary-*/*.json; do
+    if [[ $config == $bestconfig ]]; then continue; fi
+    echo "reproducing results for config $config"
+    python -m trainNN.train "$config"
+    version="$(getOutVersion $config)"
     echo "evaluation $version"
-    python -m evaluate.evaluate "trainNN/out/$version/config.json"
+    python -m evaluate.evaluate "trainNN/out/$version/config.json" bestmargin
 done
 
-echo "See the Section 'Evaluation Visualizer' in the readme to see the results"
